de48228ef27a3a27dad586b13e3c089b5b6fb543
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2020-06-18 21:26:43 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:14.4.0-alpine3.12
+
+WORKDIR /home/app
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from 'next';
+
+const Index: NextPage = () => <div>Welcome Next.js</div>;
+
+export default Index;
