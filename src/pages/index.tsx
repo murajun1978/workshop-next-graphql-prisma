07cee027a1541fef9d991e632dbdcb91c33d98e0
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2020-06-20 16:22:25 +0900

    Add toto list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
 import { NextPage } from 'next';
+import { Fragment } from 'react';
+import { useTodosQuery } from '../graphql/client/generated/graphql';
 
-const Index: NextPage = () => <div>Welcome Next.js</div>;
+const Index: NextPage = () => {
+  const { data, loading, error } = useTodosQuery();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  return (
+    <ul>
+      {data.todos.map((todo) => (
+        <Fragment key={todo.id}>
+          <li>{todo.title}</li>
+          <input
+            type="checkbox"
+            checked={todo.done}
+            onChange={() => {}}
+          ></input>
+        </Fragment>
+      ))}
+    </ul>
+  );
+};
 
 export default Index;
